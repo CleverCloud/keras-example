68e2b308fb22a6fe2af1f7f66ca950926b39b36d
Victor ballu <victor.ballu@clever-cloud.com>
2019-06-18 17:29:25 +0200

    test

--- start.sh
+++ start.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-while [ true ] ; do
-sleep 10
-done
-
 git clone https://github.com/titu1994/Neural-Style-Transfer.git
 
 python get_bucket_content.py $BUCKET_SOURCE $(pwd)/source_images
