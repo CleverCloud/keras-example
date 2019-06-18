029728139daeac6442275f0229ad105753c28e7c
Victor ballu <victor.ballu@clever-cloud.com>
2019-06-18 17:46:51 +0200

    test

--- start.sh
+++ start.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-while [ true ] ; do
-sleep 10
-done
-
 git clone https://github.com/titu1994/Neural-Style-Transfer.git
 
 python get_bucket_content.py $BUCKET_SOURCE $(pwd)/source_images
@@ -25,7 +21,7 @@ for filename in $(pwd)/../source_images/*.jpg; do
 base_filename=$(basename $filename)
 prefix=${base_filename::-4}
 mkdir results/$prefix
-python Network.py $filename  $style results/$prefix/$prefix  --num_iter 1  --content_loss_type 0 --image_size 800 --model "vgg19" --rescale_image "yes"
+python Network.py $filename  $style results/$prefix/$prefix  --num_iter 1  --content_loss_type 0 --image_size 600 --model "vgg16" --rescale_image "yes"
 i=$(($i + 1))
 break
 done
